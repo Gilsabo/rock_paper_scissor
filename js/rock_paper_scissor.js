8d98e08d380f35c3965c9b2d72682318b2d8464b
Gil Sala <gilsala_88@hotmail.com>
2022-09-06 11:07:08 +0200

    add getComputerchoice function to create a random selections for the computer

---
+++ js/rock_paper_scissor.js
@@ -0,0 +1,11 @@
+let x = 'helloworld';
+console.log(x);
+
+let computerOptions = ['rock','paper','scissor'];
+
+let getComputerChoice =  Math.floor(Math.random() * computerOptions.length);
+
+let computerPlay = computerOptions[getComputerChoice];
+
+console.log (computerPlay);
+
