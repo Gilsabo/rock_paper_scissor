29342ac8819cc78c3327aa32b0ef471ac2081456
Gil Sala <gilsala_88@hotmail.com>
2022-09-07 08:47:14 +0200

    add a function that plays a single round of rock paper scissor

--- js/rock_paper_scissor.js
+++ js/rock_paper_scissor.js
@@ -4,17 +4,31 @@ console.log(x);
 let rockPaperScissorarray = ['rock','paper','scissor'];
 
 function getComputerChoice (){
-console.log( rockPaperScissorarray[ (Math.floor(Math.random() * rockPaperScissorarray.length))]);
+return ( rockPaperScissorarray[ (Math.floor(Math.random() * rockPaperScissorarray.length))]);
     
 }  
 
-getComputerChoice();
+const computerSelection = getComputerChoice();
+
+const playerSelection = 'rock';
 
 
-function playRound (playerSelection, computerPlay){
-
-
-
-
+function playRound (playerSelection, computerSelection){
+    
+    if (playerSelection == 'rock' && computerSelection == 'scissor'){
+    return 'You win, rock beats scissor';
+    }else if(playerSelection == 'rock' && computerSelection == 'paper'){
+    return 'You lose, paper beats rock';
+    }else if(playerSelection == 'scissor' && computerSelection == 'rock'){
+    return 'You lose, rock beats scissor';
+    }else if(playerSelection == 'scissor' && computerSelection == 'paper'){
+    return 'You win, scissor beats paper';
+    }else if(playerSelection == 'paper' && computerSelection == 'scissor'){
+    return 'You lose, scissor beats paper';
+    }else if(playerSelection == 'paper' && computerSelection == 'rock'){
+    return 'You win, paper beats scissor';
+     }else {
+    return 'tie, it is getting tight in here';
+    }
 }
-
+console.log(playRound(playerSelection, computerSelection));
