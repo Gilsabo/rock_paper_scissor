929e68f2589456f6029cb13f983e403e935ccb80
Gil Sala <gilsala_88@hotmail.com>
2022-09-08 10:57:36 +0200

    add to the function game a conunter in order to know who is the winner

--- js/rock_paper_scissor.js
+++ js/rock_paper_scissor.js
@@ -1,5 +1,4 @@
-let x = 'helloworld';
-console.log(x);
+
 
 let rockPaperScissorarray = ['rock','paper','scissor'];
 
@@ -17,21 +16,20 @@ function playRound (playerSelection, computerSelection){
     
     if (playerSelection == 'rock' && computerSelection == 'scissor'){
     return 'You win, rock beats scissor';
+    }else if(playerSelection == 'scissor' && computerSelection == 'paper'){
+    return 'You win, scissor beats paper';
+    }else if(playerSelection == 'paper' && computerSelection == 'rock'){
+    return 'You win, paper beats scissor';
     }else if(playerSelection == 'rock' && computerSelection == 'paper'){
     return 'You lose, paper beats rock';
     }else if(playerSelection == 'scissor' && computerSelection == 'rock'){
     return 'You lose, rock beats scissor';
-    }else if(playerSelection == 'scissor' && computerSelection == 'paper'){
-    return 'You win, scissor beats paper';
     }else if(playerSelection == 'paper' && computerSelection == 'scissor'){
     return 'You lose, scissor beats paper';
-    }else if(playerSelection == 'paper' && computerSelection == 'rock'){
-    return 'You win, paper beats scissor';
      }else {
     return 'tie, it is getting tight in here';
     }
 }
-
 
 let playerScore = 0;
 let computerScore = 0;
@@ -41,13 +39,40 @@ function game(){
     for ( let i = 0; i < 5; i++ ) {
         const computerSelection = getComputerChoice();
         playRound (playerSelection, computerSelection);
-        console.log(playRound (playerSelection, computerSelection))
-
+        if (playerSelection == 'rock' && computerSelection == 'scissor'){
+            console.log('human '+ ++playerScore + 'computer' + computerScore);
+            }else if(playerSelection == 'scissor' && computerSelection == 'paper'){
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+ ++playerScore + 'computer' + computerScore);
+            }else if(playerSelection == 'paper' && computerSelection == 'rock'){
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+ ++playerScore + 'computer' + computerScore);
+            }else if(playerSelection == 'rock' && computerSelection == 'paper'){
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+playerScore + 'computer' + ++computerScore);
+            }else if(playerSelection == 'scissor' && computerSelection == 'rock'){
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+playerScore + 'computer' + ++computerScore);
+            }else if(playerSelection == 'paper' && computerSelection == 'scissor'){
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+playerScore + 'computer' + ++computerScore);
+             }else {
+                console.log(playRound (playerSelection, computerSelection))
+                console.log('human '+ playerScore + 'computer' + computerScore);
+            }
+       
+        }
+        if (playerScore > computerScore ){
+            console.log('human wins');
+        }else if(computerScore > playerScore){
+            console.log('computer wins');
+        }else if(playerScore === computerScore){
+            console.log('draw, let\'s play revange');
+        }
     }
     
-}
         
-        
+    
                 
              
 console.log(game());
@@ -55,9 +80,3 @@ console.log(game());
             
 
 
-/*if(console.log(playerSelection, computerSelection)==='You win, rock beats scissor'||'You win, scissor beats paper'||'You win, paper beats scissor'){
-    console.log('human '+playerScore++ + 'computer' + computerScore);
-}else if(console.log(playerSelection, computerSelection)==='You lose, paper beats rock'||'You lose, rock beats scissor'||'You lose, scissor beats paper'){
-    console.log('human '+playerScore + 'computer' + computerScore++);
-}else{
-    console.log('human '+ playerScore + 'computer' + computerScore);*/
